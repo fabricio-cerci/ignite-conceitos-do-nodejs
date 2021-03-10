efe278ee08383a5d988e51b2d8ef5df86e2c7a78
Fabrício Cerci <fabriciocerci@gmail.com>
2021-03-10 01:32:43 -0400

    Implements update route for todo

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ function checksExistsUserAccount(request, response, next) {
   const user = users.find(user => user.username === username);
 
   if(!user){
-    return response.status(400).json({error: 'User not found'});
+    return response.status(404).json({error: 'User not found'});
   }
 
   request.user = user;
@@ -69,7 +69,20 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+  const { id } = request.params;
+  const { user } = request;
+
+  const todo = user.todos.find(todo => todo.id === id); 
+
+  if(!todo){
+    return response.status(404).json({error: 'Todo not found'});
+  }
+
+  todo.title = title;
+  todo.deadline = deadline;
+  
+  return response.json(todo);
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
